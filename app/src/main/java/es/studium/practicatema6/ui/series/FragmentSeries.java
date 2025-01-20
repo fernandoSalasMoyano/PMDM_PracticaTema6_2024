bb6265c6bf83c66d41bf52ef10b8d15dc07dd400
fernandoSalasMoyano <fernandosalasmoyano@gmail.com>
2025-01-20 19:00:09 +0100

    Third Commit

--- app/src/main/java/es/studium/practicatema6/ui/series/FragmentSeries.java
+++ app/src/main/java/es/studium/practicatema6/ui/series/FragmentSeries.java
@@ -17,15 +17,16 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import es.studium.practicatema6.MiAdaptador;
 import es.studium.practicatema6.R;
 
 public class FragmentSeries extends Fragment
 {
 
     private FragmentSeriesViewModel mViewModel;
-
     ListView listaSeries;
-    ArrayList<String> elementosSeries;
+    ArrayList<String> titulosSeries;
+    ArrayList<Integer> imagenesSeries;
 
     public static FragmentSeries newInstance()
     {
@@ -41,27 +42,43 @@ public class FragmentSeries extends Fragment
 
         // Inicializar la ListView
         listaSeries = root.findViewById(R.id.miLista2);
-// Inicializar el ArrayList de series
-        elementosSeries = new ArrayList<>();
-        elementosSeries.add(getString(R.string.serie_breaking_bad));
-        elementosSeries.add(getString(R.string.serie_game_of_thrones));
-        elementosSeries.add(getString(R.string.serie_stranger_things));
-        elementosSeries.add(getString(R.string.serie_the_office));
-        elementosSeries.add(getString(R.string.serie_sherlock));
-        elementosSeries.add(getString(R.string.serie_friends));
-        elementosSeries.add(getString(R.string.serie_how_i_met_your_mother));
-        elementosSeries.add(getString(R.string.serie_the_crown));
+
+        // Inicializar el ArrayList titulos de series
+        titulosSeries = new ArrayList<>();
+        titulosSeries.add(getString(R.string.serie_breaking_bad));
+        titulosSeries.add(getString(R.string.serie_game_of_thrones));
+        titulosSeries.add(getString(R.string.serie_stranger_things));
+        titulosSeries.add(getString(R.string.serie_the_office));
+        titulosSeries.add(getString(R.string.serie_sherlock));
+        titulosSeries.add(getString(R.string.serie_friends));
+        titulosSeries.add(getString(R.string.serie_how_i_met_your_mother));
+        titulosSeries.add(getString(R.string.serie_the_crown));
+
+        // Inicializar el ArrayList de imagenes de series
+        imagenesSeries = new ArrayList<>();
+        imagenesSeries.add(R.drawable.breaking_bad);
+        imagenesSeries.add(R.drawable.game_of_thrones);
+        imagenesSeries.add(R.drawable.stranger_things);
+        imagenesSeries.add(R.drawable.the_office);
+        imagenesSeries.add(R.drawable.sherlock);
+        imagenesSeries.add(R.drawable.friends);
+        imagenesSeries.add(R.drawable.how_i_met_your_mother);
+        imagenesSeries.add(R.drawable.the_crown);
 
         // Crear un Adaptador
-        ArrayAdapter<String> adaptador = new ArrayAdapter<>(requireContext(),
-                android.R.layout.simple_list_item_1, elementosSeries);
+        // Crear un Adaptador
+        MiAdaptador adaptador = new MiAdaptador(requireContext(),
+                R.layout.lista_item, titulosSeries, imagenesSeries);
+
+        // Asignar el adaptador a la ListView
+        listaSeries.setAdapter(adaptador);
 
         // Asignar el adaptador a la ListView
         listaSeries.setAdapter(adaptador);
 
         // Configurar el evento onItemClick
         listaSeries.setOnItemClickListener((parent, view, position, id) ->
-                Toast.makeText(requireContext(), "Has elegido " + elementosSeries.get(position),
+                Toast.makeText(requireContext(), "Has elegido " + titulosSeries.get(position),
                         Toast.LENGTH_LONG).show()
         );
 
